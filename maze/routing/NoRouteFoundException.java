72a21f8f9657c8583c749bf4c5b624ca80e1eb2a
Joshua Mather <joshua.mather@student.manchester.ac.uk>
2020-03-25 19:51:56 +0000

    visualise maze

--- maze/routing/NoRouteFoundException.java
+++ maze/routing/NoRouteFoundException.java
@@ -3,6 +3,7 @@ package maze.routing;
 /**
  * Exception if no route from enterance to exit
  */
+@SuppressWarnings("serial")
 public class NoRouteFoundException extends Exception{
     public NoRouteFoundException(){
         super("No Route Found");
--- maze/routing/RouteFinder.java
+++ maze/routing/RouteFinder.java
@@ -1,5 +1,51 @@
 package maze.routing;
 
-public class RouteFinder{
+import maze.*;
+import java.util.Stack;
+import java.util.List;
 
+public class RouteFinder{
+    private Maze maze;
+    private Stack<Tile> route;
+    private boolean finished;
+
+    public RouteFinder(Maze givenMaze){
+        this.maze = givenMaze;
+        route.push(maze.getEnterance());
+    }
+
+
+    public Maze getMaze(){
+        return this.maze;
+    }
+
+
+    public List<Tile> getRoute(){
+        return this.route;
+    }
+
+
+    public boolean isFinished(){
+        return this.finished;
+    }
+
+
+    // public static RouteFinder load(String string){
+
+    // }
+
+
+    public void save(String string){
+
+    }
+
+
+    public boolean step(){
+        return true;
+    }
+
+
+    public String toString(){
+        return "a";
+    }
 }
